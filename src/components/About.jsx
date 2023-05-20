22fb11720caafcdb5dc8f156a1952921182eee28
Daniel <daniel_ye.nz@hotmail.com>
2023-05-19 22:11:21 -0700

    finish overview section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -13,7 +14,21 @@ const ServiceCard = ({ index, title, icon }) => {
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
-
+      <div
+        options={{
+          max:45, 
+          scale: 1,
+          speed: 450
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+      <img 
+        src={icon}
+        alt={title}
+        className="w-16 h-16 object-contain"
+      />
+      <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+      </div>
       </motion.div>
     </Tilt>
   )
@@ -49,4 +64,5 @@ const About = () => {
   )
 }
 
-export default About
+// wrap about section in higher order component
+export default SectionWrapper(About, "about")
